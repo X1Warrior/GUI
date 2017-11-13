e54e48b4dbc79b072a8fc41598e812447cfc26c1
X1Warrior <zsto7638@gmail.com>
2017-11-13 12:05:04 -0700

    Start to make a GUI, setup the GUI itself in the GUIFrame and helped it with the controller.

--- src/gui/controller/GUIAppRunner.java
+++ src/gui/controller/GUIAppRunner.java
@@ -1,6 +1,12 @@
 package gui.controller;
 
+import gui.controller.GUIAppController;
+
 public class GUIAppRunner
 {
-
+	public static void main(String [] args)
+	{
+		GUIAppController app = new GUIAppController();
+				app.start();
+	}	
 }
