2b6d178467443d0f774989c3f87f00d23cff98df
X1Warrior <zsto7638@gmail.com>
2017-11-13 12:16:52 -0700

    Making the Panel that is where the work happens inside the frame.

---
+++ src/gui/view/GUIPanel.java
@@ -0,0 +1,13 @@
+package gui.view;
+
+import javax.swing.JPanel;
+import gui.controller.GUIAppController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+
+public class GUIPanel extends JPanel
+{
+
+}
