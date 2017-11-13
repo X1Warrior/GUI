decd8a513501f9582b055359ecba3cdcd813ffc6
X1Warrior <zsto7638@gmail.com>
2017-11-13 12:58:37 -0700

    Setup my Jpanel and sarted to use WindowBuilder to position my button.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -6,14 +6,40 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
 
-
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+	}
+	
+	/**
+	 * used to add all components to the screen and install a layout manager
+	 * also set color :D
+	 */
+	private void setupPanel()
+	{
+		this.setBackground(Color.MAGENTA);
+		this.add(firstButton);
+		this.setLayout(appLayout);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
 	}
 }
